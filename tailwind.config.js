d71ee432955008e062abf3084a2d69d850f335f8
Javlon2 <choriyevjavlon5@gmail.com>
2024-08-02 15:20:35 +0500

    javlonqwqw

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,6 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'slider-first' : " url('./src/components/Hero/assets/Rectangle 6.png')",
-        'slider-2': "url('./src/components/Hero/assets/Rectangle 226.png')",
-        'slider-3': "url('./src/components/Hero/assets/Rectangle 227.png')",
         'slider-4': "url('./src/assets/bg.png')",
         'slider-5': "url('./src/assets/bg (1).png')",
       }
